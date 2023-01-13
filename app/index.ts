4082c40c729a6200d911accc47495c27ec1d414e
Elvyn Mejia <elvynmejia@gmail.com>
2023-01-13 02:26:20 -0800

    adds morgan middleware

--- app/index.ts
+++ app/index.ts
@@ -1,9 +1,11 @@
 import express, { Express } from 'express';
 import rateRouter from './api/v1/orders/rate';
+import morgan from 'morgan';
 
 const app: Express = express();
 
 app.use(express.json());
+app.use(morgan('combined'));
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/v1/orders', rateRouter);
