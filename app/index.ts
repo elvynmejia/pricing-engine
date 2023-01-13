babdb03aa969a23d5447c10ac236b3dd5a52e44b
Elvyn Mejia <elvynmejia@gmail.com>
2023-01-13 11:17:48 -0800

    adds common morgan

--- app/index.ts
+++ app/index.ts
@@ -5,7 +5,7 @@ import morgan from 'morgan';
 const app: Express = express();
 
 app.use(express.json());
-app.use(morgan('combined'));
+app.use(morgan('common'));
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/v1/orders', rateRouter);
